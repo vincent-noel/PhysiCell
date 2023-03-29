5e09f18f1c3dffda33c7507e3706da753bfd050c
Vincent Noël <vincent.noel@curie.fr>
2023-03-29 11:09:38 +0200

    SVG plotting and legend plotting now using custom functions, set by default to new standard functions

--- modules/PhysiCell_pathology.cpp
+++ modules/PhysiCell_pathology.cpp
@@ -657,38 +657,9 @@ void SVG_plot( std::string filename , Microenvironment& M, double z_slice , doub
 	{
 		Cell* pC = (*all_cells)[i]; // global_cell_list[i]; 
   
-		static std::vector<std::string> Colors; 
 		if( fabs( (pC->position)[2] - z_slice ) < pC->phenotype.geometry.radius )
 		{
-			double r = pC->phenotype.geometry.radius ; 
-			double rn = pC->phenotype.geometry.nuclear_radius ; 
-			double z = fabs( (pC->position)[2] - z_slice) ; 
-   
-			Colors = cell_coloring_function( pC ); 
-
-			os << "   <g id=\"cell" << pC->ID << "\" " 
-			<< "type=\"" << pC->type_name << "\" "; // new April 2022  
-			if( pC->phenotype.death.dead == true )
-			{ os << "dead=\"true\" " ; } 
-			else
-			{ os << "dead=\"false\" " ; } 
-			os << ">" << std::endl; 
-  
-			// figure out how much of the cell intersects with z = 0 
-   
-			double plot_radius = sqrt( r*r - z*z ); 
-
-			Write_SVG_circle( os, (pC->position)[0]-X_lower, (pC->position)[1]-Y_lower, 
-				plot_radius , 0.5, Colors[1], Colors[0] ); 
-
-			// plot the nucleus if it, too intersects z = 0;
-			if( fabs(z) < rn && PhysiCell_SVG_options.plot_nuclei == true )
-			{   
-				plot_radius = sqrt( rn*rn - z*z ); 
-			 	Write_SVG_circle( os, (pC->position)[0]-X_lower, (pC->position)[1]-Y_lower, 
-					plot_radius, 0.5, Colors[3],Colors[2]); 
-			}					  
-			os << "   </g>" << std::endl;
+			pC->functions.plot_agent_SVG(os, pC, z_slice, cell_coloring_function, X_lower, Y_lower);
 		}
 		
 	}
@@ -770,6 +741,41 @@ void SVG_plot( std::string filename , Microenvironment& M, double z_slice , doub
  
 	return; 
 }
+
+void standard_agent_SVG(std::ofstream& os, PhysiCell::Cell* pC, double z_slice, std::vector<std::string> (*cell_coloring_function)(Cell*), double X_lower, double Y_lower) {
+
+	double r = pC->phenotype.geometry.radius ; 
+	double rn = pC->phenotype.geometry.nuclear_radius ; 
+	double z = fabs( (pC->position)[2] - z_slice) ; 
+
+	static std::vector<std::string> Colors = cell_coloring_function( pC ); 
+
+	os << "   <g id=\"cell" << pC->ID << "\" " 
+	<< "type=\"" << pC->type_name << "\" "; // new April 2022  
+	if( pC->phenotype.death.dead == true )
+	{ os << "dead=\"true\" " ; } 
+	else
+	{ os << "dead=\"false\" " ; } 
+	os << ">" << std::endl; 
+	
+	// figure out how much of the cell intersects with z = 0
+	double plot_radius = sqrt( r*r - z*z );
+
+	// then normal cell, plot sphere if it intersects z = 0;
+	Write_SVG_circle( os, (pC->position)[0]-X_lower, (pC->position)[1]-Y_lower,
+						plot_radius , 0.5, Colors[1], Colors[0] );
+	// plot the nucleus if it, too intersects z = 0;
+	if( fabs(z) < rn && PhysiCell_SVG_options.plot_nuclei == true )
+	{
+		plot_radius = sqrt( rn*rn - z*z );
+		Write_SVG_circle( os, (pC->position)[0]-X_lower, (pC->position)[1]-Y_lower,
+							plot_radius, 0.5, Colors[3],Colors[2]);
+	}
+	
+	os << "   </g>" << std::endl;
+
+}
+
 
 std::vector<std::string> paint_by_density_percentage( double concentration, double max_conc, double min_conc ){
 
@@ -891,35 +897,40 @@ void create_plot_legend( std::string filename , std::vector<std::string> (*cell_
 	
 	for( int k=0 ; k < number_of_cell_types ; k++ )
 	{
-		// switch to the cell type 
-		Cell C; 
-		C.convert_to_cell_definition( *(cell_definitions_by_index[k]) );
-		
-		// get the colors using the current coloring function 
-		std::vector<std::string> colors = cell_coloring_function(&C); 
-		
-		// place a big circle with cytoplasm colors 
-		Write_SVG_circle(os,cursor_x, cursor_y , temp_cell_radius , 1.0 , colors[1] , colors[0] ); 
-		// place a small circle with nuclear colors 
-		Write_SVG_circle(os,cursor_x, cursor_y , 0.5*temp_cell_radius , 1.0 , colors[3] , colors[2] ); 
-		
-		// place the label 
-		
-		cursor_x += temp_cell_radius + 2*padding; 
-		cursor_y += 0.3*font_size; 
-		
-		Write_SVG_text( os , cell_definitions_by_index[k]->name.c_str() , cursor_x , cursor_y, font_size , 
-			PhysiCell_SVG_options.font_color.c_str() , PhysiCell_SVG_options.font.c_str() );
-		
-		// move the cursor down to the next row 
-		
-		cursor_y -= 0.3*font_size; 
-		cursor_y += ( 2.0 * padding + 2.0*temp_cell_radius ); 
-		cursor_x = padding + temp_cell_radius;
+		Cell_Definition* cell_def = cell_definitions_by_index[k];
+		cell_def->functions.plot_agent_legend(os, cell_def, cursor_x, cursor_y, cell_coloring_function, temp_cell_radius, padding, font_size);
 	}
 	
 	Write_SVG_end( os ); 
 	os.close(); 
+}
+
+void standard_agent_legend(std::ofstream& os, Cell_Definition* cell_definition, double& cursor_x, double& cursor_y, std::vector<std::string> (*cell_coloring_function)(Cell*), double temp_cell_radius, double padding, double font_size) {
+	
+	// switch to the cell type 
+	Cell C; 
+	C.convert_to_cell_definition( *cell_definition );
+	
+	// get the colors using the current coloring function 
+	std::vector<std::string> colors = cell_coloring_function(&C); 	
+
+	// place a big circle with cytoplasm colors 
+	Write_SVG_circle(os,cursor_x, cursor_y , temp_cell_radius , 1.0 , colors[1] , colors[0] ); 
+	// place a small circle with nuclear colors 
+	Write_SVG_circle(os,cursor_x, cursor_y , 0.5*temp_cell_radius , 1.0 , colors[3] , colors[2] ); 
+	
+	// place the label
+	cursor_x += temp_cell_radius + 2*padding; 
+	cursor_y += 0.3*font_size; 
+	
+	Write_SVG_text( os , cell_definition->name.c_str() , cursor_x , cursor_y, font_size , 
+		PhysiCell_SVG_options.font_color.c_str() , PhysiCell_SVG_options.font.c_str() );
+	
+	// move the cursor down to the next row 
+	
+	cursor_y -= 0.3*font_size; 
+	cursor_y += ( 2.0 * padding + 2.0*temp_cell_radius ); 
+	cursor_x = padding + temp_cell_radius;
 }
 
 };
