ac9d606cc3d0feb1d623f5350d17eaa86aaf92fa
Vincent Noël <vincent.noel@curie.fr>
2024-08-12 15:03:25 -0300

    Trying randy's test on template

---
+++ beta/test_diff_svg.py
@@ -0,0 +1,33 @@
+# compare diffs between .svg files
+
+import os
+import sys
+import glob
+
+if (len(sys.argv) < 3):
+  usage_str = "Usage: %s <dir1> <dir2>" % (sys.argv[0])
+  print(usage_str)
+  print("e.g.:  python test_diff_svg.py ~/blah1 ~/blah2")
+  exit(1)
+else:
+   dir1 = sys.argv[1]
+   dir2 = sys.argv[2]
+
+svg_files = glob.glob(f'{dir1}/snap*.svg')
+svg_files.sort()
+if len(svg_files) == 0:
+    print("No svg files found in ",dir1)
+    exit(1)
+for filename in svg_files:
+    f = os.path.basename(filename)
+    f1 = os.path.join(dir1,f)
+    f2 = os.path.join(dir2,f)
+    cmd =  f"diff {f1} {f2} > diff_result.txt"
+    os.system(cmd)
+    with open('diff_result.txt') as f:
+        vstr = f.readlines()
+    
+        if len(vstr) == 0 or (len(vstr) == 4 and vstr[1].startswith("<    0 days") and vstr[3].startswith(">    0 days")):
+            print(filename, ": OK")
+        else:
+           exit(1)
