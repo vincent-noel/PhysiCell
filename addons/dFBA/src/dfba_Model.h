fbf279e64f42bb5a7956bbcabf318e3427ef0116
Miguel Ponce-de-Leon <miguel.ponce@bsc.es>
2024-08-29 09:45:04 +0200

    Refactoring base dfba classes

--- addons/dFBA/src/dfba_Model.h
+++ addons/dFBA/src/dfba_Model.h
@@ -30,32 +30,34 @@ LIBSBML_CPP_NAMESPACE_USE
 
 class dFBAModel
 {
-private:
-	/** \brief Constraint-Based Model Class to perform FBA*/
+	private:
+		/** \brief Constraint-Based Model Class to perform FBA*/
 
-	std::string id;
+		std::string id;
 
 		/** \brief vector of reaction objects*/
 		std::vector<dFBAMetabolite*> metabolites;
 
-	/** \brief map between metabolites' ids and metabolites' references **/
-	std::map<std::string, int> metaboliteIndexer;
+		/** \brief map between metabolites' ids and metabolites' references **/
+		std::map<std::string, int> metaboliteIndexer;
 
 		/** \brief vector of reaction objects*/
 		std::vector<dFBAReaction*> reactions;
 
-	/** \brief map between reaction IDs and reaction references */
-	std::map< std::string, int> reactionsIndexer;
+		/** \brief map between reaction IDs and reaction references */
+		std::map< std::string, int> reactionsIndexer;
 
 		/** \brief solution  */
 		dFBASolution solution;
 
-	bool is_initialized = false;
+		/** \brief Coin CLP simplex model to encode the FBA problem**/
+		ClpSimplex problem;
+		
+		CoinMessageHandler* handler;
 
-public:
+		bool is_initialized = false;
 
-	/** \brief Constructor */
-	FBA_model();
+	public:
 
 		/** \brief Constructor */
 		dFBAModel();
@@ -63,8 +65,8 @@ public:
 		/** \brief Destructor */
 		~dFBAModel();
 
-	/** \brief a metabolite pointer using a string Id*/
-	const FBA_metabolite* getMetabolite(std::string mId);
+		/** \brief Check if there is a metaboltie with a given ID*/
+		bool hasMetabolite(std::string mId);
 
 		/** \brief a metabolite pointer using a string Id*/
 		const dFBAMetabolite* getMetabolite(std::string mId);
@@ -82,14 +84,26 @@ public:
 		/** \brief Add new reaction to the model*/
 		void addReaction(dFBAReaction* rxn);
 
-	/** \brief Get the number of model reactions*/
-	const int getNumReactions();
+		/** \brief Get the integer index of a reaction*/
+		const int getReactionIndex(std::string rId);
+		
+		/** \brief Get the upper bound of a reactions*/
+		float getReactionUpperBound(std::string rId);
+		
+		/** \brief Set the upper bound of a reactions*/
+		void setReactionUpperBound(std::string rId, float upperBound);
+		
+		/** \brief Get the upper bound of a reactions*/
+		float getReactionLowerBound(std::string rId);
+		
+		/** \brief Set the lower bound of a reactions*/
+		void setReactionLowerBound(std::string rId, float lowerBound);
 
-	/** \brief Get the number of model metabolites*/
-	const int getNumMetabolites();
+		/** \brief Get the number of model reactions*/
+		const int getNumReactions();
 
-	/** \brief Get a metabolite pointer using s string Id*/
-	const std::vector<FBA_metabolite*> getListOfMetabolites() const;
+		/** \brief Get the number of model metabolites*/
+		const int getNumMetabolites();
 
 		/** \brief Get a metabolite pointer using s string Id*/
 		const std::vector<dFBAMetabolite*> getListOfMetabolites() const;
@@ -100,22 +114,23 @@ public:
 			/** \brief Get the list of reaction pointers*/
 		std::vector<dFBAReaction*> getListOfBoundaryReactions();
 
-	/** \brief Get the ClpSimplex model */
-	const ClpSimplex* getLpModel() const;
+		/** \brief Get the list of IDs of the boundary reactions*/
+		std::vector<std::string> getListOfBoundaryReactionIds();
 
-	/** \brief Parse and read a metabolic model from a SBML file*/
-	void readSBMLModel(const char* sbmlFileName);
-	
-	/** \brief Get the ClpSimplex model */
-	void initLpModel();
+		/** \brief Get the ClpSimplex model */
+		const ClpSimplex* getLpModel() const;
 
-	void initFBAmodel(const char* sbmlFileName);
+		/** \brief Parse and read a metabolic model from a SBML file*/
+		void readSBMLModel(const char* sbmlFileName);
+		
+		/** \brief Initialize the ClpSimplex model */
+		void initProblem();
 
-	/** \brief Write LP problem in MPS format */
-	void writeLp(const char *filename);
+		/** \brief Initialize the CBM model and the LP problem */
+		void initModel(const char* sbmlFileName);
 
-	/** \brief Run FBA using primal method */
-	void runFBA();
+		/** \brief Write problem in LP format */
+		void writeProblem(const char *filename);
 
 		/** \brief Run FBA using primal method */
 		dFBASolution optimize();
