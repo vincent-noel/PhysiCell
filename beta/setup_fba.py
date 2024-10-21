ecf973504c51ba40d8b77c1c22b7fa532c4f73d9
marcorusc <m.ruscone94@gmail.com>
2024-10-21 12:02:11 +0200

    improved python script to download sbml and coin-or

--- beta/setup_fba.py
+++ beta/setup_fba.py
@@ -1,95 +1,88 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
-import sys, os
+import os
 import json
-import argparse
-
-import requests
-import hashlib
+import platform
+import urllib.request
 import tarfile
 import zipfile
-import platform
+import sys
 
+def get_os_arch():
+    """Determine the operating system and architecture."""
+    os_type = platform.system().lower()
+    arch = platform.machine().lower()
 
-def param_parser():
-    parser = argparse.ArgumentParser(description="")
-    parser.add_argument('--pkg', dest="pkg", required=True, help='Available packages', choices=PACKAGES)
-    parser.add_argument('--arch', dest="arch", required=True, choices=ARCHS, help='Current arch')
-    parser.add_argument('--checksum', dest="checksum", default=False, help='Check file integrity after downloading')
-    parser.add_argument('--path', dest='path', default=DEFAULT_LIBS_PATH, help='Default folder destination to install third-party libs if changed, Makefiles you be updated according')
-    return parser
+    if os_type == 'linux':
+        return 'linux-x64' if arch in ('x86_64', 'amd64') else 'linux-x86'
+    elif os_type == 'darwin':
+        return 'osx'
+    elif os_type.startswith('win'):
+        return 'win64' if arch == 'amd64' else 'win32'
+    else:
+        print(f"Unsupported OS: {os_type}")
+        sys.exit(1)
 
+def ensure_directory_exists(path):
+    """Create directory if it does not exist."""
+    if not os.path.exists(path):
+        os.makedirs(path)
 
-PACKAGES = ("coin-or", "libsbml")
-ARCHS = ("linux-x64", "linux-x86", "win64", "win32", "osx")
-OS_TYPE = platform.system()
-SBML_DIR = "libsbml"
+def download_and_extract(url, dest_path, is_zip=False):
+    """Download and extract a package from a URL."""
+    filename = url.split('/')[-1]
+    file_path = os.path.join(dest_path, filename)
 
-current_folder = os.path.abspath(os.path.dirname(__file__))
-BASE_FOLDER = os.path.abspath(os.path.join(current_folder, os.pardir))
-DEFAULT_LIBS_PATH = os.path.join(BASE_FOLDER, "addons", "dFBA", "ext")
+    # Download the file
+    print(f"Downloading {filename} from {url}...")
+    urllib.request.urlretrieve(url, file_path)
 
+    # Extract the file
+    print(f"Extracting {filename}...")
+    if is_zip:
+        with zipfile.ZipFile(file_path, 'r') as zip_ref:
+            zip_ref.extractall(dest_path)
+    else:
+        with tarfile.open(file_path, 'r:gz') as tar_ref:
+            tar_ref.extractall(dest_path)
+
+    # Clean up
+    os.remove(file_path)
+    print(f"{filename} installed successfully.\n")
 
 def main():
-    parser = param_parser()
-    args = parser.parse_args()
-   
-    json_packages = os.path.join(BASE_FOLDER, 'beta', 'fba_packages.json')
-    packages_dict = {}
+    # Define paths
+    current_folder = os.path.abspath(os.path.dirname(__file__))
+    base_folder = os.path.abspath(os.path.join(current_folder, os.pardir))
+    default_libs_path = os.path.join(base_folder, "addons", "dFBA", "ext")
+
+    # Load package information
+    json_packages = os.path.join(base_folder, 'beta', 'fba_packages.json')
     with open(json_packages) as fh:
         packages_dict = json.load(fh)
-    
-    base_path = args.path
-    if not os.path.exists(base_path):
-        print(f"Creating {base_path} folder", end=" ")
-        os.makedirs(base_path)
-        print("Ok!")
 
-    pkg_path = os.path.join(base_path, args.pkg)
-    if not os.path.exists(pkg_path):
-        print(f"Creating {pkg_path} folder", end=" ")
-        os.makedirs(pkg_path)
-        print("Ok!")
+    # Determine OS and architecture
+    arch = get_os_arch()
 
-        print("Moving to %s folder" % pkg_path)
-        os.chdir(pkg_path)
-            
-        pkg_dict = packages_dict[args.pkg][args.arch]
-        print(f"Fetching package: {args.pkg} ({args.arch})", end="")
-        print(f"Downaling from: {pkg_dict['url']}")
-        r = requests.get(pkg_dict['url'], allow_redirects=True)
+    # Install packages
+    for pkg in ("coin-or", "libsbml"):
+        pkg_path = os.path.join(default_libs_path, pkg)
 
-        # if args.checksum:
-        #     print("Package cheksum(sha256)", end=" ")
-        #     hash_strn = hashlib.sha256(r.content).hexdigest()
-        #     assert pkg_dict['sha256'] == hash_strn
-        #     print("Ok!")
-        
-        fname = pkg_dict["version"]
-        with open(fname, 'wb') as fh:
-            fh.write(r.content)
+        # Skip download if package folder already exists and is populated
+        # to add: create check if coin-or and libsbml folder exist, if they do not not, create them and then download the files
+        if os.path.exists(pkg_path) and os.listdir(pkg_path):
+            print(f"Package {pkg} is already installed. Skipping...")
+            continue
 
-        print("Extracting package in %s... " % args.pkg, end=" ")
-        if fname.endswith("zip"):
-            archiver = zipfile.ZipFile(fname, 'r')
-            archiver.extractall()
-        elif fname.endswith("tar.gz") or fname.endswith("gz"):
-            archiver = tarfile.open(fname, "r:gz")
-            archiver.extractall()
-            archiver.close()
-            if args.pkg == 'libsbml':
-                old_lib_dir = os.path.commonprefix(archiver.getnames())
-                os.rename(old_lib_dir, SBML_DIR)
-                if os.path.exists(old_lib_dir):
-                    os.rmdir(old_lib_dir)
-        os.remove(fname)
-            
-        print("Ok!")
-        print(f"Dependency {args.pkg} retrived and installed correctly :-)\n")
-    
-    else:
-        print(f"Package {args.pkg} already installed. To reinstall them remove the ext folder and run again.")
+        pkg_dict = packages_dict.get(pkg, {}).get(arch)
+        if not pkg_dict:
+            print(f"No package information found for {pkg} on {arch}. Skipping...")
+            continue
+
+        url = pkg_dict['url']
+        is_zip = url.endswith("zip")
+        download_and_extract(url, pkg_path, is_zip)
 
 if __name__ == "__main__":
     main()
